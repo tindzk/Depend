f2f1f868fc4c273600686897b4e53d9fe5a06d71
Tim Nieradzik <tim.nieradzik@gmx.de>
2010-10-15 12:26:29 +0200

    Utils: Drop `$' macro.

--- Utils.h
+++ Utils.h
@@ -3,8 +3,6 @@
 #import <Integer.h>
 #import <Terminal.h>
 
-#define $(s) String(s)
-
 extern Logger logger;
 
 void Utils_OnLogMessage(__unused void *ptr, String msg, Logger_Level level, String file, int line);
